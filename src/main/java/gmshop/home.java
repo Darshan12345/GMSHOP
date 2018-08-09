3877963e64ae97babde216cc626edd07c0a031e2
darshan1234 <darshanmakvana@gmail.com>
2018-08-08 23:19:35 -0500

    Initial commit

---
+++ src/main/java/gmshop/home.java
@@ -0,0 +1,14 @@
+package gmshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class home {
+
+
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+}
